af5075227f0f52dc11ec42b876fc74e80175f99e
israel.cortes <israel.cortes@gigigo.com.mx>
2017-06-04 17:54:32 -0500

    + Mejora en implementacion de pila

--- LibMandalaWebclient/app/src/main/java/com/mandala/webclient/client/Dispacher.java
+++ LibMandalaWebclient/app/src/main/java/com/mandala/webclient/client/Dispacher.java
@@ -6,85 +6,78 @@ import android.view.View;
 import com.mandala.webclient.interfaces.DispacherListener;
 import com.mandala.webclient.interfaces.RequestInterface;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by israel on 16/04/16.
  */
 public class Dispacher {
 
-    private static Integer MAX_THREADS = 3;
-    private static Map<String, RequestTask> queueActive = new HashMap<>();
-    private static Map<String, RequestTask> queuePending = new HashMap<>();
+    private static int MAX_THREADS = 2;
+    private static RequestTask activeTask = null;
+    private static Queue<String> queueMax = new LinkedList<>();
+    private static Queue<RequestTask> queue = new LinkedList<>();
 
-    public static void setMaxThreads(Integer maxThreads) {
+    public static void setMaxThreads(int maxThreads) {
         MAX_THREADS = maxThreads;
     }
 
     public static void sendRequest(RequestInterface request){
         RequestTask requestTask = new RequestTask(request);
         requestTask.setDispacherListener(getDispacherListener());
-        execute(requestTask);
+        addToQueue(requestTask);
     }
 
     public static void sendRequest(View viewLoader, RequestInterface request){
         RequestTask requestTask = new RequestTask(viewLoader, request);
         requestTask.setDispacherListener(getDispacherListener());
-        execute(requestTask);
+        addToQueue(requestTask);
     }
 
     public static void sendRequest(Dialog dialogLoader, RequestInterface request){
         RequestTask requestTask = new RequestTask(dialogLoader, request);
         requestTask.setDispacherListener(getDispacherListener());
-        execute(requestTask);
+        addToQueue(requestTask);
     }
 
     private static DispacherListener getDispacherListener(){
         return new DispacherListener(){
             @Override
             public void onRequestEnd(RequestTask task) {
-                queueActive.remove(task.getRequest().getUrl());
-                if(queuePending.size() > 0){
-                    RequestTask pendingTask = getNextPending();
-                    execute(pendingTask);
-                }
+                executeQueue();
             }
         };
     }
 
-    private static void execute(RequestTask task){
-        if(queueActive.size() < MAX_THREADS) {
-            if(queuePending.containsKey(task.getRequest().getUrl()))
-                queuePending.remove(task.getRequest().getUrl());
-            queueActive.put(task.getRequest().getUrl(), task);
-            task.execute();
-        }else{
-            queuePending.put(task.getRequest().getUrl(), task);
+    private static void addToQueue(RequestTask task){
+        if(queueMax.size() <= MAX_THREADS){
+            queueMax.add(String.valueOf(task.hashCode()));
+            queue.add(task);
+            executeQueue();
+        }else
+            queue.add(task);
+    }
+
+    private static void executeQueue(){
+        queueMax.poll();
+        activeTask = queue.poll();
+        if(activeTask != null){
+            if(!activeTask.getRequest().isCanceled())
+                activeTask.execute();
+            else
+                executeQueue();
         }
     }
 
-    public static boolean isActive(RequestInterface request){
-        return queueActive.containsKey(request.getUrl());
-    }
+    private static void cancelAllRequest(){
+        if(queue.size() > 0)
+            queue.clear();
 
-    public static boolean isPending(RequestInterface request){
-        return queuePending.containsKey(request.getUrl());
-    }
+        if(queueMax.size() > 0)
+            queueMax.clear();
 
-    public static void cancelRequest(RequestInterface request){
-        request.setCancel(true);
-        if(isPending(request))
-            queuePending.remove(request.getUrl());
+        if(activeTask != null)
+            activeTask.getRequest().setCancel(true);
     }
-
-    private static RequestTask getNextPending(){
-        for(Iterator<Map.Entry<String, RequestTask>> it = queuePending.entrySet().iterator(); it.hasNext(); ) {
-            Map.Entry<String, RequestTask> entry = it.next();
-            return entry.getValue();
-        }
-        return null;
-    }
-
 }
