6774402a1ff9fb88eb43a57fa13349c012548964
Israel Cortes <israel.cortes@gigigo.com.mx>
2017-08-13 21:23:30 -0500

    Restoring sample activity

--- LibMandalaWebclient/app/src/main/java/com/mandala/test/ActivityTest.java
+++ LibMandalaWebclient/app/src/main/java/com/mandala/test/ActivityTest.java
@@ -9,15 +9,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.mandala.test.R;
-//import com.mandala.test.model.ResponseGenre;
-//import com.mandala.webclient.client.Dispacher;
-//import com.mandala.webclient.client.WebClient;
-//import com.mandala.webclient.interfaces.ClientConfigs;
-//import com.mandala.webclient.interfaces.RequestConfigs;
-//import com.mandala.webclient.model.ResponseInfo;
-//
-//import okhttp3.OkHttpClient;
-//import okhttp3.Request;
+import com.mandala.test.model.ResponseGenre;
+import com.mandala.webclient.client.Dispacher;
+import com.mandala.webclient.client.WebClient;
+import com.mandala.webclient.interfaces.ClientConfigs;
+import com.mandala.webclient.interfaces.RequestConfigs;
+import com.mandala.webclient.model.ResponseInfo;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * Created by just_ on 01/05/2017.
@@ -35,37 +35,37 @@ public class ActivityTest extends AppCompatActivity {
         layItems = (LinearLayout) findViewById(R.id.lay_items);
         textStatus = (TextView) findViewById(R.id.text_status);
 
-//        WebClient.getInstance().setContext(this);
-//        WebClient.getInstance().setActiveLog(BuildConfig.DEBUG);
-//        WebClient.getInstance().setClientConfigs(new ClientConfigs<OkHttpClient.Builder>() {
-//            @Override
-//            public void configClient(OkHttpClient.Builder client) {
-//                //Instala certificados ssl
-//            }
-//        });
-//        WebClient.getInstance().setRequestConfigs(new RequestConfigs<Request.Builder>() {
-//            @Override
-//            public void configRequest(Request.Builder request) {
-//                //Agrega parametros o confuguraciones de cada peticion por ejemplo tokens o cookies
-//            }
-//        });
-//
-//        findViewById(R.id.button_call).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                final View itmView = inflateItem();
-//                layItems.addView(itmView);
-//
-//                Dispacher.sendRequest(itmView.findViewById(R.id.loader), new RequestGenres(){
-//                    @Override
-//                    public void onComplete(ResponseInfo response, ResponseGenre content) {
-//                        textStatus.setText(Dispacher.getStatus());
-//                        ((TextView)itmView.findViewById(R.id.text_display)).setText("Request "+content.toString()+" done");
-//                    }
-//                });
-//                textStatus.setText(Dispacher.getStatus());
-//            }
-//        });
+        WebClient.getInstance().setContext(this);
+        WebClient.getInstance().setActiveLog(BuildConfig.DEBUG);
+        WebClient.getInstance().setClientConfigs(new ClientConfigs<OkHttpClient.Builder>() {
+            @Override
+            public void configClient(OkHttpClient.Builder client) {
+                //Instala certificados ssl
+            }
+        });
+        WebClient.getInstance().setRequestConfigs(new RequestConfigs<Request.Builder>() {
+            @Override
+            public void configRequest(Request.Builder request) {
+                //Agrega parametros o confuguraciones de cada peticion por ejemplo tokens o cookies
+            }
+        });
+
+        findViewById(R.id.button_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final View itmView = inflateItem();
+                layItems.addView(itmView);
+
+                Dispacher.sendRequest(itmView.findViewById(R.id.loader), new RequestGenres(){
+                    @Override
+                    public void onComplete(ResponseInfo response, ResponseGenre content) {
+                        textStatus.setText(Dispacher.getStatus());
+                        ((TextView)itmView.findViewById(R.id.text_display)).setText("Request "+content.toString()+" done");
+                    }
+                });
+                textStatus.setText(Dispacher.getStatus());
+            }
+        });
     }
 
     public View inflateItem(){
